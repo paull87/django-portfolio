d7ffcfbd8a50b128d1cc0b291dab112848451f5b
paull <paul.lucas@secretescapes.com>
2019-04-15 09:08:32 +0100

    Added blog html pages

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,7 @@ def blog_index(request):
 
 def blog_category(request, category):
     posts = Post.objects.filter(
-        categories_name_contains=category
+        categories__name__contains=category
     ).order_by('-created_on')
     context = {
         "category": category,
