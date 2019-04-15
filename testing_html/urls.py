71c55b918132fb62b77db3db60a4bb5986670f64
paull <paul.lucas@secretescapes.com>
2019-04-15 17:29:48 +0100

    Div and Span

--- testing_html/urls.py
+++ testing_html/urls.py
@@ -4,4 +4,5 @@ from testing_html import views
 urlpatterns = [
     path("", views.basics, name="basics"),
     path("lists/", views.lists, name="lists"),
+    path("divspans/", views.div_spans, name="divs_spans"),
 ]
--- testing_html/views.py
+++ testing_html/views.py
@@ -4,5 +4,10 @@ from django.shortcuts import render
 def basics(request):
     return render(request, 'basics.html', {})
 
+
 def lists(request):
     return render(request, 'lists.html', {})
+
+
+def div_spans(request):
+    return render(request, 'div_spans.html', {})
