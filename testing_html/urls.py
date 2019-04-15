df4f387426cc84b9d89ed47a4ea39dfe62795343
paull <paul.lucas@secretescapes.com>
2019-04-15 17:10:58 +0100

    Lists examples

--- testing_html/urls.py
+++ testing_html/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from testing_html import views
 
 urlpatterns = [
-    path("", views.basics, name="basics")
+    path("", views.basics, name="basics"),
+    path("lists/", views.lists, name="lists"),
 ]
--- testing_html/views.py
+++ testing_html/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def basics(request):
     return render(request, 'basics.html', {})
+
+def lists(request):
+    return render(request, 'lists.html', {})
